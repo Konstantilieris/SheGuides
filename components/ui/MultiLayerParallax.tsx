d22bf598cf06ab0e22ea56054eeb6d96a63b6067
Lomachenko <aggeloskonstantilieris@gmail.com>
2024-09-11 19:15:06 +0300

    second view

--- components/ui/MultiLayerParallax.tsx
+++ components/ui/MultiLayerParallax.tsx
@@ -8,23 +8,40 @@ import {
   useInView,
 } from "framer-motion";
 import { IconArrowRight } from "@tabler/icons-react";
+import { useMediaQuery } from "react-responsive";
+import { cn } from "@/lib/utils";
 const MultiLayerParallax = () => {
   const ref = useRef(null);
   const refView = useRef(null);
-  const { scrollYProgress } = useScroll({
+  const { scrollYProgress, scrollY } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
   });
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+
   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-  const textY = useTransform(scrollYProgress, [0, 1], ["-100vh", "250vh"]);
+  const textY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [isPortrait ? "-80vh" : "-80vh", isPortrait ? "115vh" : "210vh"]
+  );
   const opacityText = useTransform(
     scrollYProgress,
-    [0, 0.1, 0.125, 0.2, 0.3],
-    [0, 0.2, 0.4, 0.6, 1]
+    [0, 0.1, 0.125, 0.2, 0.3, 0.4, 0.6],
+    [0, 0.2, 0.3, 0.4, 0.6, 0.8, 1]
   );
-  useEffect(() => {
-    console.log(scrollYProgress);
-  }, [scrollYProgress]);
+
+  const largeOpacity = useTransform(
+    scrollYProgress,
+    [0, 0.1, 0.125, 0.2, 0.3],
+    [0, 0.2, 0.5, 0.7, 1]
+  );
+
   const inView = useInView(refView, { once: true });
   const mainControls = useAnimation();
   const slideControls = useAnimation();
@@ -33,21 +50,30 @@ const MultiLayerParallax = () => {
       mainControls.start("visible");
       slideControls.start("visible");
     }
-  }, [inView]);
+  }, [inView, isPortrait, scrollYProgress.get()]);
   return (
     <div
-      className="w-full min-h-[160vh] overflow-hidden relative flex items-center justify-center flex-col"
+      className="w-full min-h-[160vh] overflow-hidden relative flex items-center justify-center flex-col max-sm:min-h-[200vh]"
       ref={ref}
     >
       <motion.h1
-        className=" text-light-900 text-8xl relative z-30 font-serif lg:tracking-widest max-lg:text-7xl max-md:mb-20 md:mb-40 lg:mb-20 2xl:mb-0 max-sm:mb-40"
-        style={{ y: textY, opacity: opacityText }}
+        className=" text-light-900 text-8xl relative z-30 font-serif lg:tracking-widest max-lg:text-7xl max-md:mb-20 md:mb-40 lg:mb-20 2xl:mb-0  "
+        style={{
+          y: textY,
+          opacity: isDesktopOrLaptop ? largeOpacity : opacityText,
+        }}
       >
         SheGuides
       </motion.h1>
       <div
         ref={refView}
-        className="lg:max-w-[45vw] absolute lg:bottom-48 md:bottom-80 overflow-hidden z-30 max-lg:fit-content max-lg:px-4  text-center max-lg:text-light-800 lg:text-sky-200 max-md:bottom-52 max-md:text-light-900 xl:mb-20"
+        className={cn(
+          " absolute  overflow-hidden z-30  text-center max-lg:text-light-800 lg:text-sky-200  max-sm:bottom-[54vh] lg:bottom-[28vh] max-lg:bottom-[34vh] max-md:bottom-[32vh] xl:max-w-[50vw] lg:max-w-[74vw]",
+          {
+            " max-w-[66vw]": isTabletOrMobile,
+            " max-w-[90vw] !important": isPortrait,
+          }
+        )}
       >
         <motion.span
           className="text-2xl  font-serif"
@@ -95,7 +121,7 @@ const MultiLayerParallax = () => {
         }}
       />
 
-      <motion.button className=" flex flex-row items-center gap-2 font-serif font-bold text-lg shadow-[0_8px_24px_0_rgb(0,0,0,10%)] hover:shadow-[0_24px_50px_rgba(93,93,93,83%)] px-8 py-2 bg-[#fff] text-[#696969] rounded-md  transition duration-200 ease-linear z-30 absolute bottom-28 ">
+      <motion.button className=" flex flex-row items-center gap-2 font-serif font-bold text-lg shadow-[0_8px_24px_0_rgb(0,0,0,10%)] hover:shadow-[0_24px_50px_rgba(93,93,93,83%)] px-8 py-2 bg-[#fff] text-[#696969] rounded-md  transition duration-200 ease-linear z-30 absolute bottom-28 hover:text-dark-400">
         Book Now <IconArrowRight />
       </motion.button>
     </div>
